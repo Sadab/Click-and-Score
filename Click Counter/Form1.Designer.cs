4f845901f0cc94ee54bb6c198de1f4f34c893d40
Sadab Rahman Michael <sadabrahman@gmail.com>
2017-12-12 18:36:44 +0600

    Add project files.

---
+++ Click Counter/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Click_Counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.scoreLbl = new System.Windows.Forms.Label();
+            this.scoreLblTxt = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelHow = new System.Windows.Forms.Label();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkCyan;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Harrington", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 87);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click me!!!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkCyan;
+            this.button2.Font = new System.Drawing.Font("Harrington", 10.8F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(194, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 87);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Click me!!!";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkCyan;
+            this.button3.Font = new System.Drawing.Font("Harrington", 10.8F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(12, 135);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 87);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Click me!!!";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkCyan;
+            this.button4.Font = new System.Drawing.Font("Harrington", 10.8F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(194, 135);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 87);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Click me!!!";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.DarkCyan;
+            this.button5.Font = new System.Drawing.Font("Harrington", 10.8F, System.Drawing.FontStyle.Bold);
+            this.button5.Location = new System.Drawing.Point(12, 238);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 87);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Click me!!!";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.DarkCyan;
+            this.button6.Font = new System.Drawing.Font("Harrington", 10.8F, System.Drawing.FontStyle.Bold);
+            this.button6.Location = new System.Drawing.Point(194, 238);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 87);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Click me!!!";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.DarkCyan;
+            this.button7.Font = new System.Drawing.Font("Harrington", 10.8F, System.Drawing.FontStyle.Bold);
+            this.button7.Location = new System.Drawing.Point(12, 339);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(150, 87);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Click me!!!";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.DarkCyan;
+            this.button8.Font = new System.Drawing.Font("Harrington", 10.8F, System.Drawing.FontStyle.Bold);
+            this.button8.Location = new System.Drawing.Point(194, 339);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(150, 87);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Click me!!!";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.DarkCyan;
+            this.button9.Font = new System.Drawing.Font("Harrington", 10.8F, System.Drawing.FontStyle.Bold);
+            this.button9.Location = new System.Drawing.Point(12, 446);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(150, 87);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Click me!!!";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.DarkCyan;
+            this.button10.Font = new System.Drawing.Font("Harrington", 10.8F, System.Drawing.FontStyle.Bold);
+            this.button10.Location = new System.Drawing.Point(194, 446);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(150, 87);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Click me!!!";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // scoreLbl
+            // 
+            this.scoreLbl.Font = new System.Drawing.Font("Hobo Std", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLbl.ForeColor = System.Drawing.Color.Crimson;
+            this.scoreLbl.Location = new System.Drawing.Point(187, 555);
+            this.scoreLbl.Name = "scoreLbl";
+            this.scoreLbl.Size = new System.Drawing.Size(150, 40);
+            this.scoreLbl.TabIndex = 10;
+            // 
+            // scoreLblTxt
+            // 
+            this.scoreLblTxt.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLblTxt.Font = new System.Drawing.Font("Hobo Std", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLblTxt.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.scoreLblTxt.Location = new System.Drawing.Point(12, 555);
+            this.scoreLblTxt.Name = "scoreLblTxt";
+            this.scoreLblTxt.Size = new System.Drawing.Size(150, 40);
+            this.scoreLblTxt.TabIndex = 11;
+            this.scoreLblTxt.Text = "Score :";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelHow
+            // 
+            this.labelHow.Font = new System.Drawing.Font("Trajan Pro", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHow.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelHow.Location = new System.Drawing.Point(14, 607);
+            this.labelHow.Name = "labelHow";
+            this.labelHow.Size = new System.Drawing.Size(173, 23);
+            this.labelHow.TabIndex = 12;
+            this.labelHow.Text = "How to Play :";
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.instructionLabel.Location = new System.Drawing.Point(15, 630);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(302, 46);
+            this.instructionLabel.TabIndex = 13;
+            this.instructionLabel.Text = "Click on the button when the \r\nbutton color is green.\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(360, 705);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.labelHow);
+            this.Controls.Add(this.scoreLblTxt);
+            this.Controls.Add(this.scoreLbl);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Click Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label scoreLbl;
+        private System.Windows.Forms.Label scoreLblTxt;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelHow;
+        private System.Windows.Forms.Label instructionLabel;
+    }
+}
+
---
+++ Click Counter/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Click_Counter
+{
+    public partial class Form1 : Form
+    {
+        int score = 0;
+        int del = 0;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            //randomColorGenerator();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.BackColor == Color.Green) {
+                addScore();
+            }
+            else if (button1.BackColor == Color.DarkCyan)
+            {
+                minusScore();
+            }
+            //button1.BackColor = Color.DarkCyan; 
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.BackColor == Color.Green)
+            {
+                addScore();
+            }
+            else if (button2.BackColor == Color.DarkCyan)
+            {
+                minusScore();
+            }
+            // button2.BackColor = Color.DarkCyan; 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.BackColor == Color.Green)
+            {
+                addScore();
+            }
+            else if (button3.BackColor == Color.DarkCyan)
+            {
+                minusScore();
+            }
+            // button3.BackColor = Color.DarkCyan;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.BackColor == Color.Green)
+            {
+                addScore();
+            }
+            else if (button4.BackColor == Color.DarkCyan)
+            {
+                minusScore();
+            }
+            // button4.BackColor = Color.DarkCyan; 
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.BackColor == Color.Green)
+            {
+                addScore();
+            }
+            else if (button5.BackColor == Color.DarkCyan)
+            {
+                minusScore();
+            }
+            // button5.BackColor = Color.DarkCyan; 
+        }   
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.BackColor == Color.Green)
+            {
+                addScore();
+            }
+            else if (button6.BackColor == Color.DarkCyan)
+            {
+                minusScore();
+            }
+            //button6.BackColor = Color.DarkCyan;
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button7.BackColor == Color.Green)
+            {
+                addScore();
+            }
+            else if (button7.BackColor == Color.DarkCyan)
+            {
+                minusScore();
+            }
+            //button7.BackColor = Color.DarkCyan;
+                
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button8.BackColor == Color.Green)
+            {
+                addScore();
+            }
+            else if (button8.BackColor == Color.DarkCyan)
+            {
+                minusScore();
+            }
+            //button8.BackColor = Color.DarkCyan;
+                
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button9.BackColor == Color.Green)
+            {
+                addScore();
+            }
+            else if (button9.BackColor == Color.DarkCyan)
+            {
+                minusScore();
+            }
+            //button9.BackColor = Color.DarkCyan;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (button10.BackColor == Color.Green)
+            {
+                addScore();
+            }
+            else if (button10.BackColor == Color.DarkCyan)
+            {
+                minusScore();
+            }
+            //button10.BackColor = Color.DarkCyan;
+        }
+        
+        public void addScore() {
+            score += 10;
+            scoreLbl.Text = score.ToString();
+        }
+        public void minusScore() {
+            score -= 10;
+            scoreLbl.Text = score.ToString();
+        }
+        public void randomColorGenerator() {
+            int ajaira = random.Next(1, 11);
+            del = ajaira;
+            if (ajaira == 1) { button1.BackColor = Color.Green; }
+            else if(ajaira == 2) { button2.BackColor = Color.Green; }
+            else if(ajaira == 3) { button3.BackColor = Color.Green; }
+            else if(ajaira == 4) { button4.BackColor = Color.Green; }
+            else if(ajaira == 5) { button5.BackColor = Color.Green; }
+            else if(ajaira == 6) { button6.BackColor = Color.Green; }
+            else if (ajaira == 7) { button7.BackColor = Color.Green; }
+            else if (ajaira == 8) { button8.BackColor = Color.Green; }
+            else if (ajaira == 9) { button9.BackColor = Color.Green; }
+            else if (ajaira == 10) { button10.BackColor = Color.Green; }
+            timer1.Stop();
+            timer1.Start();
+        }
+        public void colorRestore()
+        {
+            if (del == 1) { button1.BackColor = Color.DarkCyan; }
+            else if (del == 2) { button2.BackColor = Color.DarkCyan; }
+            else if (del == 3) { button3.BackColor = Color.DarkCyan; }
+            else if (del == 4) { button4.BackColor = Color.DarkCyan; }
+            else if (del == 5) { button5.BackColor = Color.DarkCyan; }
+            else if (del == 6) { button6.BackColor = Color.DarkCyan; }
+            else if (del == 7) { button7.BackColor = Color.DarkCyan; }
+            else if (del == 8) { button8.BackColor = Color.DarkCyan; }
+            else if (del == 9) { button9.BackColor = Color.DarkCyan; }
+            else if (del == 10) { button10.BackColor = Color.DarkCyan; }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            colorRestore();
+            //timer1.Stop();
+            timer1.Start();
+            randomColorGenerator();
+        }
+    }
+}
